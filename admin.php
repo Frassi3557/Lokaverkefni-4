155bc6bf2c5ced885443883621f23d81cd145f4c
eggertaxels <eggertgeir@gmail.com>
2017-04-24 15:56:47 +0000

    uptade eftir frí

--- admin.php
+++ admin.php
@@ -1,8 +1,17 @@
-<?php require('includes/session.php'); ?>
+<?php
+if (!session_id()) session_start();
+if (!$_SESSION['logon']){ 
+    header("Location:index.php");
+    die();
+}//http://www.mustbebuilt.co.uk/php/insert-update-and-delete-with-pdo/
+?>
 <!DOCTYPE html>
 <html lang="">
-<?php require('includes/header.php');
-  ('includes/head.php'); ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
 
 <body>
     <form action="process/signout.php" method="post">
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
-  
-  <?php require('includes/head.php'); ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lokaverkefni</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+  <?php require('includes/header.php'); ?>
 
     <div class="skrainn">
         <div class="log">
