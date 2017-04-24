8be40226c070d7566eca2b17bc1afa4c9f8b2291
Frassi3557 <freysi_freys@live.com>
2017-04-24 16:28:33 +0000

    includea session.php svo eigi þurfi að skrifa út session kóðan í hvert einasta skjal.

--- breyta.php
+++ breyta.php
@@ -1,10 +1,4 @@
-<?php
-if (!session_id()) session_start();
-if (!$_SESSION['logon']){ 
-    header("Location:index.php");
-    die();
-}//http://www.mustbebuilt.co.uk/php/insert-update-and-delete-with-pdo/
-?>
+<?php require('includes/session.php'); ?>
 <!DOCTYPE html>
 <html lang="">
 <head>
