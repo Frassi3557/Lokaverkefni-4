e3b45beb93a9982bd8724c197351c4693ead14f8
eggertaxels <eggertgeir@gmail.com>
2017-04-24 16:20:22 +0000

    laga error ef þú ert ekki loggaður inn

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 if (!session_id()) session_start();
-if (!$_SESSION['logon']){ 
+if (!$_SESSION['logon'] = false){ 
     header("Location:index.php");
     die();
 }//http://www.mustbebuilt.co.uk/php/insert-update-and-delete-with-pdo/
