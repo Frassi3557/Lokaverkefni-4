494ab7afe47552fd0e2577c33ff087c1ff38d534
Frassi3557 <freysi_freys@live.com>
2017-04-25 08:31:02 +0000

    includea head.php

--- admin.php
+++ admin.php
@@ -1,12 +1,7 @@
 <?php require('includes/session.php'); ?>
 <!DOCTYPE html>
 <html lang="">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-
+  <?php require('includes/head.php'); ?>
 <body>
     <?php require('includes/header.php'); ?>
     <form action="process/signout.php" method="post">
