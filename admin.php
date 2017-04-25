8a972aaa17ef12069d94cca9741e1f0e10296d53
eggertaxels <eggertgeir@gmail.com>
2017-04-25 09:42:10 +0000

    bæta við undirsíður og breyta. virkar ekki ennþá en bætt við href í header

--- admin.php
+++ admin.php
@@ -4,6 +4,7 @@
   <?php require('includes/head.php'); ?>
 <body>
     <?php require('includes/header.php'); ?>
+    <h1><?php echo "<h1> Velkominn " . $_SESSION['name'] . ".</h1><h2> Mailið þitt er " . $_SESSION['email'] . "</h2>"; ?></h1>
     <form action="process/signout.php" method="post">
         <input type="submit">
     </form>
---
+++ undirsidur/breyta.php
@@ -0,0 +1,15 @@
+<?php require('includes/session.php'); ?>
+<!DOCTYPE html>
+<html lang="">
+  <?php require('includes/head.php'); ?>
+<body>
+    <?php require('includes/header.php'); ?>
+    <form action="../process/breytanafn.php" method="post"><br>
+           Nýtt nafn<br>
+        <input type="text" name="newname" required><br>
+           Lykilorð<br>
+        <input type="password" name="pass_ga" required><br>
+        <input value="Accept" type="submit">
+    </form>
+</body>
+</html>
