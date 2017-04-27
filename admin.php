8c395b634a54e14bf7a625f2ac8caf88e7a0503d
eggertaxels <eggertgeir@gmail.com>
2017-04-27 14:47:00 +0000

    Test með veður API bara console log. Það virkar

--- admin.php
+++ admin.php
@@ -4,9 +4,12 @@
   <?php require('includes/head.php'); ?>
 <body>
     <?php require('includes/header.php'); ?>
-    <h1><?php echo "<h1> Velkominn " . $_SESSION['name'] . ".</h1><h2> Mailið þitt er " . $_SESSION['email'] . "</h2>"; ?></h1>
+    <h1><?php echo "<h1> Velkominn " . $_SESSION['name'] . ". Hér er veðrið í dga.</h1>"; ?></h1>
+    <br>
+    <h1 class="stadsetning"></h1>
     <form action="process/signout.php" method="post">
         <input type="submit">
     </form>
+    <script src="java.js"></script>
 </body>
 </html>
--- java.js
+++ java.js
@@ -1,10 +1,14 @@
+var stadsetning = document.getElementById("stadsetning");
 $.ajax({
   'url': 'http://apis.is/weather/forecasts/',
   'type': 'GET',
   'dataType': 'json',
   'data': {'stations': '1,422'},
   'success': function(response) {
-    console.log(response);
+      //console.log(response.results[0].name);
+      if (response.results[0].name === "Reykjavík") {
+        //document.getElementById("stadsetning").innerHTML = "Good day";
+          console.log("Þú ert í Reykjavík");
+      }
   }
 });
-response.results[the thing you want];
