a2bff40984348dae7739d5de9c6d240e98a56690
eggertaxels <eggertgeir@gmail.com>
2017-05-02 09:18:14 +0000

    link bætt við í header: bilaleit

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <body>
   <ul>
-    <li><a href=undirsidur/breyta.php>1</a></li>
-    <li><a href=index.php>2</a></li>
+    <li><a href="undirsidur/breyta.php">Breyta Upplýsingum</a></li>
+    <li><a href="undirsidur/bilaleit.php">Bílaleit</a></li>
     <li><a href=index.php>3</a></li>
   </ul>
