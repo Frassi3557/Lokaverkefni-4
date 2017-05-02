53718ec993151cd5634eba8715b352d1c8c4f0e0
Snorri Freyr Ásgeirsson <Snorri Freyr sgeirsson>
2017-05-02 21:50:59 +0000

    bæti við title á nokkrar undirsíður í head

--- includes/head.php
+++ includes/head.php
@@ -5,6 +5,12 @@
     $title = 'Innskráning';
     $icon = "https://cdn3.iconfinder.com/data/icons/glypho-free/64/home-128.png";
   }
+  if ($title == 'bilanidurstodur') {
+    $title = 'Niðurstöður';
+  }
+  if ($title == 'bilaleit') {
+    $title = 'Leit bifreiða';
+  }
   $title = ucfirst($title);
 ?>
 <head>
