bd9c6673fee6381c5479ff742f59bbf5ee986c63
eggertaxels <eggertgeir@gmail.com>
2017-05-04 15:05:25 +0000

    veðurspá nánast komin með myndum

--- admin.php
+++ admin.php
@@ -6,14 +6,15 @@
     <?php require('includes/header.php'); ?>
     <h1><?php echo "<h1> Velkominn " . $_SESSION['name'] . ". Hér er veðrið í dag.</h1>"; ?></h1>
     <br>
-    <div class="vedur">
+    <div id="vedur">
         <h1 id="stadsetning"></h1>
         <h2 id="dagsetning"></h2>
         <h2 id="vedurupp"></h2>
         <h1 id="hiti"></h1>
+        <div id="icon"></div>
     </div>
     <form action="process/signout.php" method="post">
-        <input type="submit">
+        <input type="submit" value="Logout">
     </form>
     <script src="java.js"></script>
 </body>
--- java.js
+++ java.js
@@ -2,7 +2,7 @@ var stadsetning = document.getElementById("stadsetning");
 var dagsetning = document.getElementById("stadsetning");
 var vedurupp = document.getElementById("vedurupp");
 var gradur = document.getElementById("hiti");
-var vedurlysing = response.results[0].forecast[5].W;
+var mynd = document.getElementById("icon");
 $.ajax({
   'url': 'http://apis.is/weather/forecasts/',
   'type': 'GET',
@@ -15,5 +15,29 @@ $.ajax({
       dagsetning.innerHTML = response.results[0].forecast[5].ftime;
       vedurupp.innerHTML = "Veðurlýsing yfir daginn: " + response.results[0].forecast[5].W;
       gradur.innerHTML = "Hiti yfir daginn: " + response.results[0].forecast[5].T + "°C";
+      if(response.results[0].forecast[5].W === "Heiðskírt")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/421f0c16071075.562a500e6ddd4.gif'>";
+      }
+      else if(response.results[0].forecast[5].W === "Skýjað" || response.results[0].forecast[5].W === "Alskýjað")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/d241d716071075.562a50091d914.gif'>";
+      }
+      else if(response.results[0].forecast[5].W === "Léttskýjað")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/c81fed16071075.562a501d5e911.gif'>";
+      }
+      else if(response.results[0].forecast[5].W === "Rigning" || response.results[0].forecast[5].W === "Lítils háttar rigning")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/7205bf16071075.562a50132e33e.gif'>";
+      }
+      else if(response.results[0].forecast[5].W === "Snjókoma" || response.results[0].forecast[5].W === "Lítils háttar snjókoma" || response.results[0].forecast[5].W === "Snjóél")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/a2405c16071075.562a5050beed0.gif'>";
+      }
+      else if(response.results[0].forecast[5].W === "Þoka")
+      {
+          mynd.innerHTML = "<img class='vedurmyndin' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/4d353e16071075.562a504473c5c.gif'>";
+      }
   }
 });
