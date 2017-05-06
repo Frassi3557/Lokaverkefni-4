3eb88231d4449e3dda9caf7d95583384cce6930a
Snorri Freyr Ásgeirsson <Snorri Freyr sgeirsson>
2017-05-06 01:01:54 +0000

    Tek út ip töluna mín svo ég geti unnið á síðunni

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
       var blockedIP = new Array();
       
       blockedIP[0] = "x.x.x.x";
-      blockedIP[1] = "31.209.229.95";
+      blockedIP[1] = "x.x.x.x";
       blockedIP[2] = "x.x.x.x";
       
       for(var i=0; i<blockedIP.length; i++){
@@ -23,6 +23,5 @@
           break;
           
         }
-        
       }
     </script>
