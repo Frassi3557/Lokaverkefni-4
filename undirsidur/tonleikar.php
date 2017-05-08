b7047226d463a1d91be2fe84f5e17a7872364c19
Eggert Axelsson <Eggert Axelsson>
2017-05-08 02:27:19 +0000

    flest öll gögn birt, útlit eftir

--- undirsidur/tonleikar.php
+++ undirsidur/tonleikar.php
@@ -19,7 +19,13 @@ echo "lol";
   ?>
   <div>
       <?php foreach ($json_object->results as $value): ?>
-        <p id="type">Tegund: <?php echo $value->eventDateName ?></p>
+				<div class="group">
+					<h1>Event: <?php echo $value->eventDateName ?></h1>
+					<h2>Flytjendur: <?php echo $value->userGroupName ?></h1>
+						<h2>Nafn á viðburði: <?php echo $value->name ?></h1>
+						<h1>Staður: <?php echo $value->eventHallName ?></h1>
+							<img src="<?php echo $value->imageSource ?>">
+				</div>
 				<br>
       <?php endforeach; ?>  
   </div>
