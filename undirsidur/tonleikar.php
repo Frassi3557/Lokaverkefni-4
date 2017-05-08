125c058ac8bb6013de8ecb03b00e1178c0df58e7
Eggert Axelsson <Eggert Axelsson>
2017-05-08 01:57:10 +0000

    náði loks að birta gögn frá tónleikum, nú bara bæta við gögnum

--- undirsidur/tonleikar.php
+++ undirsidur/tonleikar.php
@@ -1,1 +1,28 @@
-\***
+<?php
+    
+
+  // slóð á API, sem skilar JSON
+  $url ="http://apis.is/concerts";          
+  
+  // JSON sótt úr API.
+  $json = file_get_contents($url);
+
+  // fáum php object 
+  $json_object = json_decode($json);
+echo "lol";
+?>
+
+<!DOCTYPE html>
+<html>
+  <?php require('../includes/head.php'); 
+        include('../includes/header.php');
+  ?>
+  <div>
+      <?php foreach ($json_object->results as $value): ?>
+        <p id="type">Tegund: <?php echo $value->eventDateName ?></p>
+				<br>
+      <?php endforeach; ?>  
+  </div>
+  
+</body>
+  </html> 
